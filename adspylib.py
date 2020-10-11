458bd65426ba23c9a8e4e618f7632997eb05f4d9
Patrick Freel <oriesta@gmail.com>
2020-10-11 15:06:30 -0700

    First Commit

---
+++ adspylib.py
@@ -0,0 +1,15 @@
+#%%
+print("Welcome to the adspylib package!")
+#%%
+class work_item:
+
+    #def __init__(self, parent, id, type="story", state="new", points=0, title, assignee,  \
+    #             team, created_date, backlog, closed_date, tags):
+    def __init__(self):
+        pass
+
+# %%
+if __name__ == "__main__":
+
+    print("Running adspylib unit tests...")
+    print("Done!")
